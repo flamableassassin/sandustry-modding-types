ff73d84eefe9cd1e98523520cfc5bf10d7800421
flammableassassin <43076116+flamableassassin@users.noreply.github.com>
2026-08-22 18:47:08 +0100

    Adding linting

---
+++ .prettierrc.ts
@@ -0,0 +1,14 @@
+
+
+import { type Config } from "prettier";
+
+const config: Config = {
+  trailingComma: "none",
+  tabWidth: 2,
+  useTabs: false,
+  bracketSameLine: false,
+  endOfLine: "crlf",
+  semi: true
+};
+
+export default config;
